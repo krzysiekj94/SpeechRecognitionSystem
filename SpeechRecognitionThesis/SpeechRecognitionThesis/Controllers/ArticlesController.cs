0558c772453e1f2536e6f55572fda18ec3a74afe
krzysiekj94 <k.jerzynski1@gmail.com>
2019-04-20 18:31:56 +0200

    - add articles controller with test methods

---
+++ SpeechRecognitionThesis/SpeechRecognitionThesis/Controllers/ArticlesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpeechRecognitionThesis.Controllers
+{
+    [Route("Articles")]
+    public class ArticlesController : Controller
+    {
+        [Route("Newest")]
+        public IActionResult GetNewestArticlesContent()
+        {
+            return View("Index");
+        }
+
+        [Route("Top-5")]
+        public IActionResult GetTopArticlesContent()
+        {
+            return View("Article");
+        }
+    }
+}
