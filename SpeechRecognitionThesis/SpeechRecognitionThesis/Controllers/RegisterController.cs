23d0011522d8625a2e53c7ff4081107b70ae4627
krzysiekj94 <k.jerzynski1@gmail.com>
2019-06-23 21:20:42 +0200

    + register controller with schema view

---
+++ SpeechRecognitionThesis/SpeechRecognitionThesis/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpeechRecognitionThesis.Models;
+using SpeechRecognitionThesis.Models.Repository;
+
+namespace SpeechRecognitionThesis.Controllers
+{
+    [Route("Register")]
+    public class RegisterController : Controller
+    {
+        private readonly IDataRepository<User> _dataRepository;
+
+        public RegisterController(IDataRepository<User> dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        public IActionResult RegisterUser()
+        {
+            return View("../Home/Register");
+        }
+    }
+}
