e96592c3954ab5b4f8f509901f40212b72a7b92a
krzysiekj94 <k.jerzynski1@gmail.com>
2019-07-28 23:33:45 +0200

    - authentication method

--- SpeechRecognitionThesis/SpeechRecognitionThesis/Models/Repository/AccountRepository.cs
+++ SpeechRecognitionThesis/SpeechRecognitionThesis/Models/Repository/AccountRepository.cs
@@ -8,9 +8,18 @@ namespace SpeechRecognitionThesis.Models.Repository
 {
     public class AccountRepository : RepositoryBase<User>, IAccountRepository
     {
+        RepositoryContext _repositoryContext;
         public AccountRepository(RepositoryContext repositoryContext)
             : base(repositoryContext)
         {
+            _repositoryContext = repositoryContext;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            User user = _repositoryContext.Users.SingleOrDefault(x => x.NickName == username && x.Password == password);
+
+            return user;
         }
     }
 }
