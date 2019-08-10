6dbb4ab4e6ce3f24349517c1bdac71f1a675361c
krzysiekj94 <k.jerzynski1@gmail.com>
2019-08-10 15:04:58 +0200

    change way to set password in test data

--- SpeechRecognitionThesis/SpeechRecognitionThesis/Models/Database/SpeechRecognitonDbContext.cs
+++ SpeechRecognitionThesis/SpeechRecognitionThesis/Models/Database/SpeechRecognitonDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SpeechRecognitionThesis.Models.DatabaseModels;
+using SpeechRecognitionThesis.Models.Scripts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +27,7 @@ namespace SpeechRecognitionThesis.Models.Database
             {
                 UserId = 1,
                 NickName = "SuperBass",
-                Password = "0x07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6",
+                Password = UserTools.ConvertInputTextToSha512("SuperPasswor123"),
                 Email = "bas@gmail.com",
                 CreateAccountDate = new DateTime(2019, 05, 30).ToString(),
                 LastUpdateAccountDate = new DateTime(2019, 06, 20).ToString(),
@@ -37,7 +38,7 @@ namespace SpeechRecognitionThesis.Models.Database
            {
                UserId = 2,
                NickName = " RobertSon",
-               Password = "0x07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6",
+               Password = UserTools.ConvertInputTextToSha512("passwordTest231"),
                Email = "robert@mail.com",
                CreateAccountDate = new DateTime(2019, 05, 21).ToString(),
                LastUpdateAccountDate = new DateTime(2019, 06, 23).ToString(),
