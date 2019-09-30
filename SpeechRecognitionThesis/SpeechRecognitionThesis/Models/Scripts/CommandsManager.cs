6aca7dc1fd9003aef18b79b2d7de6b39e6143555
krzysiekj94 <k.jerzynski1@gmail.com>
2019-09-30 22:46:53 +0200

    + add commands manager preview + add new template for login/register commands preview

---
+++ SpeechRecognitionThesis/SpeechRecognitionThesis/Models/Scripts/CommandsManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeechRecognitionThesis.Models.Scripts
+{
+    public class CommandsManager
+    {
+        const string MAIN_PAGE_PATH = "/";
+        const string SEARCH_PAGE_PATH = "/search";
+        const string LOGIN_PAGE_PATH = "/login";
+        const string REGISTER_PAGE_PATH = "/register";
+
+        const string SEARCH_COMMANDS_PARTIAL_VIEW_PATH = "_SearchEngineCommandsPartialView";
+        const string MAIN_COMMANDS_PARTIAL_VIEW_PATH = "_MainCommandsPartialView";
+        const string REGISTER_COMMANDS_PARTIAL_VIEW_PATH = "_RegisterCommandsPartialView";
+        const string LOGIN_COMMANDS_PARTIAL_VIEW_PATH = "_LoginCommandsPartialView";
+
+        public static string GetCommandsPartialViewPathString( string oPagePathString)
+        {
+            string commandsPartialViewNameString = MAIN_COMMANDS_PARTIAL_VIEW_PATH;
+
+            switch( oPagePathString )
+            {
+                case SEARCH_PAGE_PATH:
+                    commandsPartialViewNameString = SEARCH_COMMANDS_PARTIAL_VIEW_PATH;
+                    break;
+                case LOGIN_PAGE_PATH:
+                    commandsPartialViewNameString = LOGIN_COMMANDS_PARTIAL_VIEW_PATH;
+                    break;
+                case REGISTER_PAGE_PATH:
+                    commandsPartialViewNameString = REGISTER_COMMANDS_PARTIAL_VIEW_PATH;
+                    break;
+                default:
+                    break;
+            }
+
+            return "CommandsPartialView/" + commandsPartialViewNameString + ".cshtml";
+        }
+    }
+}
