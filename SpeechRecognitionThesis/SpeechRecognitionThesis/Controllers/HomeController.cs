8132667fb479dc018fa30f5ae3b9f326270b5ae3
krzysiekj94 <k.jerzynski1@gmail.com>
2019-10-17 22:12:58 +0200

    + load main page data to view + add model for main page

--- SpeechRecognitionThesis/SpeechRecognitionThesis/Controllers/HomeController.cs
+++ SpeechRecognitionThesis/SpeechRecognitionThesis/Controllers/HomeController.cs
@@ -6,16 +6,121 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SpeechRecognitionThesis.Models;
+using SpeechRecognitionThesis.Models.DatabaseModels;
+using SpeechRecognitionThesis.Models.Repository;
+using SpeechRecognitionThesis.Models.Scripts;
+using SpeechRecognitionThesis.Models.ViewModels;
 
 namespace SpeechRecognitionThesis.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly IRespositoryWrapper _repositoryWrapper;
+
+        public HomeController(IRespositoryWrapper repositoryWrapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+        }
+
         [AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            IndexArticlesModel indexArticlesModel = new IndexArticlesModel();
+            FillIndexArticlesModel( indexArticlesModel );
+
+            return View( indexArticlesModel );
+        }
+
+        private void FillIndexArticlesModel(IndexArticlesModel indexArticlesModel)
+        {
+            if( indexArticlesModel != null )
+            {
+                indexArticlesModel.CurrentUser = GetUserForIndex();
+                indexArticlesModel.NewestArticleUserPairList = GetNewestArticlesForIndex();
+                indexArticlesModel.MostViewedUserArticlePairList = GetMostViewedArticlesForIndex();
+                indexArticlesModel.MyArticleList = GetMyArticleArticlesForIndex( indexArticlesModel.CurrentUser );
+            }
+        }
+
+        private List<Article> GetMyArticleArticlesForIndex( User concreteUser )
+        {
+            List<Article> myArticleList = null;
+            IEnumerable<UserArticles> userArticleEnumerable = null;
+
+            if( IsCorrectUser( concreteUser ) )
+            {
+                userArticleEnumerable = _repositoryWrapper.UserArticles.GetUserArticles( concreteUser.Id );
+
+                if(userArticleEnumerable != null 
+                    && userArticleEnumerable.Count() > 0 )
+                {
+                    myArticleList = _repositoryWrapper.Articles.GetArticles( userArticleEnumerable );
+                }
+            }
+
+            return myArticleList != null ? myArticleList : new List<Article>();
+        }
+
+        private bool IsCorrectUser(User concreteUser)
+        {
+            return concreteUser != null
+                && ( (concreteUser.Id > -1)
+                       || ( concreteUser.Id == -1
+                            && concreteUser.NickName == UserTools.ANONYMOUS_USER_NICKNAME) );
+        }
+
+        private List<ArticleUserPair> GetMostViewedArticlesForIndex()
+        {
+            const int iNumberOfArticleResult = 3;
+
+            List<Article> mostViewedArticleList = _repositoryWrapper.Articles
+                                                  .GetMostViewedArticles( iNumberOfArticleResult );
+
+            List<ArticleUserPair> mostViewedArticleUserPairList = UserTools.GetUserArticlePair(_repositoryWrapper, mostViewedArticleList);
+
+
+            return (mostViewedArticleUserPairList != null ) ? mostViewedArticleUserPairList : new List<ArticleUserPair>();
+        }
+
+        private List<ArticleUserPair> GetNewestArticlesForIndex()
+        {
+            const int iNumberOfArticleResult = 3;
+            List<Article> newestArticleList = _repositoryWrapper.Articles
+                                              .GetNewestArticles( iNumberOfArticleResult );
+            List<ArticleUserPair> newestArticleUserPairList = UserTools.GetUserArticlePair(_repositoryWrapper, newestArticleList);
+
+            return ( newestArticleUserPairList != null ) ? newestArticleUserPairList : new List<ArticleUserPair>();
+        }
+
+
+        private List<ArticleUserPair> GetNewestArticlesList()
+        {
+            const int iNumberOfArticlesResult = 10;
+            List<Article> newestArticles = _repositoryWrapper.Articles.GetNewestArticles(iNumberOfArticlesResult);
+            List<ArticleUserPair> newestArticleList = UserTools.GetUserArticlePair(_repositoryWrapper, newestArticles);
+
+            return newestArticleList;
+        }
+
+        private User GetUserForIndex()
+        {
+            long lUserId = -1;
+            User currentUser = null;
+
+            if( long.TryParse(TokenProvider.GetRegisterUserPropertyString(User.Identity, UserTools.USER_ID_PROPERTY_STRING), out lUserId))
+            {
+                currentUser = _repositoryWrapper.Account.GetUser(lUserId);
+            }
+            else
+            {
+                currentUser = _repositoryWrapper.Account.GetAnonymousUser();
+            }
+
+            currentUser.Password = string.Empty;
+            currentUser.Id = -1;
+
+            return currentUser;
         }
 
         [AllowAnonymous]
---
+++ SpeechRecognitionThesis/SpeechRecognitionThesis/Models/ViewModels/IndexArticlesModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeechRecognitionThesis.Models.ViewModels
+{
+    public class IndexArticlesModel
+    {
+        public List<ArticleUserPair> NewestArticleUserPairList { get; set; }
+        public List<ArticleUserPair> MostViewedUserArticlePairList { get; set; }
+        public List<Article> MyArticleList { get; set; }
+        public List<Article> RecommendedArticle { get; set; }
+        public User CurrentUser { get; set; }
+    }
+}
